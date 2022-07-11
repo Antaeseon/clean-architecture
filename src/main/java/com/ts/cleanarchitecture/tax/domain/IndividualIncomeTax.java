0724845f402aaa0670ee3f7a1745dec0849e64ed
Antaeseon <ants412@naver.com>
2022-07-12 01:18:18 +0900

    feat: 종합소득세 domain 정의

---
+++ src/main/java/com/ts/cleanarchitecture/tax/domain/IndividualIncomeTax.java
@@ -0,0 +1,51 @@
+package com.ts.cleanarchitecture.tax.domain;
+
+public class IndividualIncomeTax {
+
+	/*
+	종합소득세 id
+	 */
+	private Long id;
+
+	/*
+	종합소득세 userId
+	 */
+	private Long userId;
+	/*
+	매출액
+	 */
+	private Long revenue;
+	/*
+	소득금액
+	 */
+	private Long incomeAmount;
+	/*
+	소득공제
+	 */
+	private Long incomeDeduction;
+	/*
+	과세표준
+	 */
+	private Long taxBase;
+	/*
+	산출세액
+	 */
+	private Long caculatedTax;
+	/*
+	세액공제
+	 */
+	private Long taxCredit;
+	/*
+	가산세
+	 */
+	private Long penalty;
+	/*
+	기납부세액
+	 */
+	private Long prepaidTax;
+
+	/*
+	납부할세액
+	 */
+	private Long taxPayment;
+}
