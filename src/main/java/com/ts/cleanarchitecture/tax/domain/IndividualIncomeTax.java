412efe9a7c6d9375db5318b879912ab901481ac3
Antaeseon <ants412@naver.com>
2022-07-12 02:11:21 +0900

    feat: 종합소득세 생성시 재무제표 값 입력 로직 추가

--- src/main/java/com/ts/cleanarchitecture/tax/domain/IndividualIncomeTax.java
+++ src/main/java/com/ts/cleanarchitecture/tax/domain/IndividualIncomeTax.java
@@ -1,5 +1,7 @@
 package com.ts.cleanarchitecture.tax.domain;
 
+import com.ts.cleanarchitecture.tax.application.port.in.CreateTaxCommand;
+
 public class IndividualIncomeTax {
 
 	/*
@@ -58,4 +60,51 @@ public class IndividualIncomeTax {
 	납부할세액
 	 */
 	private Long taxPayment;
+
+	public boolean setFinancial(CreateTaxCommand command) {
+		this.revenue = command.getRevenue();
+		this.userId= command.getUserId();
+		this.infoType=command.getInfoType();
+		return calculateIncomeAmount(command);
+	}
+
+	/*
+	소득금액 계산 ( 매출액 - 필요경비[ 주요경비 + 기타경비 ] )
+	 */
+	private boolean calculateIncomeAmount(CreateTaxCommand command) {
+		this.incomeAmount = this.revenue - command.getCost();
+		return incomeAmount > 0;
+	}
+
+
+	//TODO : 각각 세금 계산 로직 추가
+	/*
+	소득공제액 세팅
+	 */
+
+	/*
+	과세표준 금액 세팅
+	 */
+
+	/*
+	산출세액 금액 세팅
+	 */
+
+	/*
+	세액공제액 금액 세팅
+	 */
+
+	/*
+	가산세 금액 세팅
+	 */
+
+	/*
+	기납부세액 금액 세팅
+	 */
+
+	/*
+	납부할 세액 금액 세팅
+	 */
+
+
 }
