2bad026cc7a47783c8c233499748c12c18f55e5f
Antaeseon <ants412@naver.com>
2022-08-05 00:20:22 +0900

    test: Adapter Testcase 추가

---
+++ src/test/java/com/ts/cleanarchitecture/tax/adapter/in/web/CreateTaxControllerTest.java
@@ -0,0 +1,48 @@
+package com.ts.cleanarchitecture.tax.adapter.in.web;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.web.servlet.MockMvc;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.ts.cleanarchitecture.tax.adapter.in.dto.CreateTaxDto;
+import com.ts.cleanarchitecture.tax.application.port.in.CreateTaxCommand;
+import com.ts.cleanarchitecture.tax.application.port.in.CreateTaxUseCase;
+import com.ts.cleanarchitecture.tax.domain.IndividualIncomeTax;
+
+import static org.mockito.BDDMockito.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+import javax.validation.constraints.NotNull;
+
+@WebMvcTest(controllers = CreateTaxController.class)
+class CreateTaxControllerTest {
+
+	@Autowired
+	ObjectMapper objectMapper;
+
+	@Autowired
+	private MockMvc mockMvc;
+
+	@MockBean
+	private CreateTaxUseCase createTaxUseCase;
+
+	@Test
+	void testCreateTax() throws Exception {
+		CreateTaxDto request = new CreateTaxDto(1L, "type", 1000L, 500L);
+
+		mockMvc.perform(post("/tax")
+			.header("Content-Type", "application/json")
+			.content(this.objectMapper.writeValueAsString(request)))
+			.andExpect(status().isOk());
+
+		// then(createTaxUseCase).should()
+		// 	.createTax(eq(new CreateTaxCommand(1L, "type", 100L, 500L)));
+	}
+
+}
